896b9da16ad6cab2ba411a050817fb8a88cfa150
Ifrat Binte <ifratbinte@gmail.com>
2023-02-27 15:02:38 +0600

    feat(*): add particle animation and update some design of home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,15 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
     container: {
-      padding: '2rem',
+      padding: "2rem",
     },
   },
   plugins: [],
-}
+};
