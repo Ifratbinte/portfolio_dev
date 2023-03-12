2dd107e35ce25ec2d8d08409fda8d81bf431c10e
Ifrat Binte <ifratbinte@gmail.com>
2023-03-12 15:31:27 +0600

    feat  : use react lightbox

--- src/components/lightbox/lightbox.tsx
+++ src/components/lightbox/lightbox.tsx
@@ -1,7 +1,7 @@
-import LightboxComponent, {LightboxExternalProps} from "yet-another-react-lightbox";
+import LightboxComponent, { LightboxExternalProps } from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 import photos from "./photos";
 
 export default function Lightbox(props: LightboxExternalProps) {
-  return <LightboxComponent slides={photos} {...props} />;
+  return <LightboxComponent  slides={photos} {...props} /> 
 }
