c45ca5b90f11dfbe38f84dfc456f129b76e2460b
Ifrat Binte <ifratbinte@gmail.com>
2023-03-13 09:57:30 +0600

    feat(typedjs*) : integrate react-typed plugin

---
+++ src/config/TypedJs/typedComponent.tsx
@@ -0,0 +1,22 @@
+import Typed from "react-typed";
+const TypedComponent = () => {
+  return (
+    <div>
+      <Typed 
+          strings={[
+            "I'm Frontend Designer",
+            "I'm React Developer", 
+            "I'm Software Engineer"
+            ]} 
+            typeSpeed={40} 
+            backSpeed={50} 
+            attr="placeholder" 
+            loop
+            >  
+        <input type="text" className="border-0 bg-transparent text-3xl !outline-none text-center" />
+      </Typed>
+    </div>
+  );
+};
+
+export default TypedComponent;
