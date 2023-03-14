eb726a3c69309f229e3e94549b236f4a924922ef
Ifrat Binte <ifratbinte@gmail.com>
2023-03-14 18:21:46 +0600

    feat : font-family integrate

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,15 +11,26 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'sea-green': '#009e66',
+        "sea-green": "#009e66",
       },
       backgroundImage: {
-        'hero-bg' : "url('/images/bg-hero.jpg')"
-      }
+        "hero-bg": "url('/images/bg-hero.jpg')",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
     },
     container: {
       padding: "2rem",
     },
+    // container: {
+    //   padding: {
+    //     sm: "2rem",
+    //     lg: "4rem",
+    //     xl: "5rem",
+    //     2xl: "6rem",
+    //   },
+    // },
   },
   plugins: [],
 };
